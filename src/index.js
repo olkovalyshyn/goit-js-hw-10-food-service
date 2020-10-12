4e92e3166d24b48de9240998f33d90ffb1559d99
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-10-13 02:10:23 +0300

    Добавляє код для зміни теми

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import "./css/styles.css";
 
-console.log("Мастерская: деплой билда от Parcel на GitHub Pages");
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const refs = {
+    body: document.querySelector('body'),
+    switch: document.querySelector('#theme-switch-toggle'),
+};
+
+refs.switch.addEventListener('change', onBtnChange);
+ 
+const isChecked = refs.switch.checked;
+
+
+function onBtnChange() {
+    if (isChecked) {
+        refs.body.classList.add(Theme.DARK);
+    }
+    else {
+        refs.body.classList.add(Theme.LIGHT);
+
+    }
+};
