b7b1e6aca46817cb5a4409838cda622f24a913b0
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-10-14 01:04:49 +0300

    Добавляє ДЗ

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+import cardsTemplates from "./templates/cards-food.hbs";
+import cardsData from "./menu.json";
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
 const refs = {
     body: document.querySelector('body'),
     switch: document.querySelector('#theme-switch-toggle'),
+    menu: document.querySelector('.js-menu'),
 };
 
 refs.switch.addEventListener('change', onBtnChange);
- 
-const isChecked = refs.switch.checked;
 
+const isChecked = refs.switch.checked;
 
 function onBtnChange() {
     if (isChecked) {
@@ -22,3 +25,7 @@ function onBtnChange() {
 
     }
 };
+
+
+const template = templating.compile(cardsTemplates);
+refs.menu.insertAdjacentHTML('beforeend', template(cardsData));
